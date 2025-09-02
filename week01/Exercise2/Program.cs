c1289dec1605764a92824a5d116b688515de1d40
ethanatkinson <ethanatkinson11@icloud.com>
2025-09-02 15:29:37 -0600

    Completed Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+
+        if (number >= 90)
+        {
+            Console.WriteLine($"Your grade is: {number}, A");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine($"Your grade is: {number}, B");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine($"Your grade is: {number}, C");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine($"Your grade is: {number}, D");
+        }
+        else
+        {
+           Console.WriteLine($"Your grade is: {number}, F"); 
+        }
+        
     }
 }
