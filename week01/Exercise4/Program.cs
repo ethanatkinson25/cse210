a14bac23c504033469d9f4278e113aa8f21f7c2e
ethanatkinson <ethanatkinson11@icloud.com>
2025-09-04 11:16:02 -0600

    Completed Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a numbet to add it to the list.Type 0 to end list.");
+        Console.WriteLine();
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();
+        int newNumber = int.Parse(input);
+        int count = 1;
+        int sum = newNumber;
+        int greatest = newNumber;
+
+        while (newNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            input = Console.ReadLine();
+            newNumber = int.Parse(input);
+            numbers.Add(newNumber);
+            count = count + 1;
+            sum = sum + newNumber;
+        }
+
+        foreach (int number in numbers)
+        {
+            if (number > greatest)
+            {
+                greatest = number;
+            }
+        }
+
+        float average = sum / count;
+        Console.WriteLine($"The total sum is: {sum}");
+        Console.WriteLine($"The average of the list is: {average}");
+        Console.WriteLine($"The greatest number in your list is: {greatest}");
+        Console.WriteLine($"The total amount of items in your list is: {count} items");
     }
 }
