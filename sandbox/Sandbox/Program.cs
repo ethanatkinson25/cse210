c62cc7f22f87bf39c5c8e0226ce893243fd51b9c
ethanatkinson <ethanatkinson11@icloud.com>
2025-09-07 22:48:55 -0600

    Completed Abstraction Assignment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        //This is the object
+        //An object is collection of characterisitcs(variables) and behaviors(methods) needed to complete a task
+        //Objects can be put into variables, lists, and other objects
+        var Blind = new Blind();
+        
+        //This executes the behavior of the object.  The ".GetArea" is the behavior method of the object
+        double blindArea = Blind.GetArea();
+
+        Console.WriteLine(blindArea);
+    }
+
+    //Class
+    //The blueprint to creating an object
+    public class Blind
+    {
+        //Attributes
+        //These are characteristics that an object needs to complete a task
+        //Sometimes these can be called member variables
+        //Member variables can have complex data types and be an object within themselves
+        //It's standard practice to start the name of a member variable with a "_"
+        public double _width;
+        public double _height;
+        public string _color;
+
+        //Behaviors
+        //The behavoiors that an object needs to do to complete a task
+        public double GetArea()
+        {
+            return _width * _height;
+        }
     }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.WriteLine();
+
+        var job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+        job1.displayJob();
+
+        var job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Apple";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+        job2.displayJob();
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        var myResume = new Resume();
+        Console.WriteLine();
+        myResume._name = "Ethan Atkinson";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.displayResume();
+    }
+
+    public class Job
+    {
+        //Keeps track of the company, job title, start year, and end year.
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+        public void displayJob()
+        {
+            Console.WriteLine($"{_jobTitle}, ({_company}) {_startYear}-{_endYear}");
+            Console.WriteLine();
+        }
+    }
+
+    public class Resume
+    {
+        //Keeps track of the person's name and a list of their jobs.
+        //Will contain Job objects as member variables
+
+        public string _name;
+        public List<Job> _jobs = new List<Job>();
+
+
+        public void displayResume()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs: ");
+
+            foreach (var item in _jobs)
+            {
+                Console.WriteLine($"{item._jobTitle}, ({item._company}) {item._startYear}-{item._endYear}");
+
+            }
+            Console.WriteLine();
+        }
     }
 }
