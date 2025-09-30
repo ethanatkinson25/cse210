9b755dde660ab83ddf6004b7764ae1de3c496719
ethanatkinson <ethanatkinson11@icloud.com>
2025-09-30 16:41:32 -0600

    Completed Inheritance activity

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+
+public class Assignment
+{
+    protected string _studentName;
+    private string _topic;
+
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string getSummary()
+    {
+        return $"{_studentName}, Topic: {_topic}";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Ethan", "Science");
+        MathAssignment mathAssignment = new MathAssignment("Ethan", "Math", "Section 5", "1-10");
+        WritingAssignment writingAssignment = new WritingAssignment("Ethan", "English", "A Guide to a Well Written Essay");
+
+        Console.WriteLine(assignment.getSummary());
+        Console.WriteLine();
+        Console.WriteLine(mathAssignment.getSummary());
+        Console.WriteLine(mathAssignment.getHomeworkList());
+        Console.WriteLine();
+        Console.WriteLine(writingAssignment.getSummary());
+        Console.WriteLine(writingAssignment.getWritingInfo());
     }
 }
